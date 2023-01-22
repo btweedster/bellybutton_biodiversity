f928980897bbaad1ab57616ed31db6830fd730da
btweedster <benjamin@Bens-Mac.local>
2023-01-22 01:01:24 -0500

    testing change

--- charts.js
+++ charts.js
@@ -60,7 +60,8 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     console.log(data);
 
-    // Deliverable 1: 3. Create a variable that holds the samples array. 
+    // Deliverable 1: 3. Create a variable that holds the samples array.
+    console.log("hello");
 
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
 
@@ -78,7 +79,7 @@ function buildCharts(sample) {
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = 
+    var yticks = [];
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
     var barData = [
